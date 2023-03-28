b67268aabecbfee1504b5ddfe230d0d6ae56d425
Braulio Gabriel Dumba <brauliodumba@braulios-mbp.mynetworksettings.com>
2023-03-28 11:51:26 -0400

    Adding dev-test env

---
+++ environments/dev-env/README.md
@@ -0,0 +1,18 @@
+## Quickstart
+
+## Required Packages:
+   - ko: https://ko.build/install/
+   - gcc
+   - jq
+   - go (version expected 1.19 or higher)
+   - kind
+   - kubectl  
+
+## 
+
+1. Run the following script:
+
+```
+./install_edge-mc.sh
+
+```
---
+++ environments/dev-env/install_edge-mc.sh
@@ -0,0 +1,13 @@
+
+# (1): Clone the kcp-playground tool
+git clone https://github.com/fabriziopandini/kcp.git
+
+# (2): Clone the edge-mc repo
+git clone https://github.com/kcp-dev/edge-mc.git
+
+# cd kcp
+cd environments/dev-env
+
+# # (3): Move the kcp-playground yaml files to the target repo
+
+# mv stage1 
