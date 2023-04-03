0b7daf668a457fe4a8efdcc0c5a5c6c02fb114be
Braulio Gabriel Dumba <brauliodumba@braulios-mbp.mynetworksettings.com>
2023-04-03 15:18:31 -0400

    latest version

--- environments/dev-env/delete_edge-mc.sh
+++ environments/dev-env/delete_edge-mc.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
+clusters="florin guilder"
+
+while (( $# > 0 )); do
+    if [ "$1" == "--clusters" ]; then
+        clusters=$2
+    fi 
+    shift
+done
+
+# Deleting kind clusters
+for c in ${clusters[@]}
+do 
+  if kind get clusters | grep $c; then
+     kind delete cluster --name $c
+     echo "Deleting kind cluster: $c"
+  fi
+done
+
+
 pkill kubectl-kcp-playground
 pkill kcp
 pkill mailbox-controller
 pkill placement-translator
 pkill main # edge-scheduler
-kind delete cluster --name florin
-kind delete cluster --name guilder
 rm -rf $(pwd)/kcp
+
+rm placement-translator-log.txt
+rm edge-scheduler-log.txt
+rm mailbox-controller-log.txt
+rm kcp-playground-log.txt
+rm -rf $(pwd)/kcp
+
 echo "Finished deletion ...."
--- environments/dev-env/install_edge-mc.sh
+++ environments/dev-env/install_edge-mc.sh
@@ -3,16 +3,24 @@
 set -e
 
 stage=""
+clusters="florin guilder"
+verbosity=0
 
 while (( $# > 0 )); do
     if [ "$1" == "--stage" ]; then
         stage=$2
+        shift
+    elif [ "$1" == "--clusters" ]; then
+        clusters=$2
+        shift
+    elif [ "$1" == "-v" ]; then
+        verbosity=1
     fi 
     shift
 done
 
 # Check if docker is running
-if ! docker ps
+if ! docker ps > /dev/null
 then
   echo "Docker Not running ...."
   exit
@@ -21,29 +29,105 @@ fi
 # Check go version
 go_version=`go version | { read _ _ v _; echo ${v#go}; }`
 
-if [ $go_version != "1.19" ]
-then
-  echo "Go version must be 1.19 ...."
-  exit
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+if [ $(ver $go_version) -lt $(ver 1.19) ]; then
+    echo "Update your go version"
 fi
+
+# Deleting kind clusters
+for c in ${clusters[@]}
+do 
+  if kind get clusters | grep $c; then
+     kind delete cluster --name $c
+  fi
+done
+
+# KCP is an older kcp-edge deployment is already running
+process_running() {
+  SERVICE="$1"
+  if pgrep -x "$SERVICE" >/dev/null
+  then
+      echo "running"
+  else
+      echo "stopped" 
+  fi
+}
+
+# Check kcp-edge is already running
+if [ $(process_running kcp) == "running" ]
+then
+    echo "An older deployment of kcp-playground is already running - terminating it ...."
+    pkill kubectl-kcp-playground
+    pkill kcp
+fi
+
+# Check mailbox-controller is already running
+if [ $(process_running mailbox-controller) == "running" ]
+then
+    echo "An older deployment of mailbox-controller is already running - terminating it ...."
+    pkill mailbox-controller
+fi
+
+# Check edge-scheduler is already running
+if [ $(process_running main) == "running" ]
+then
+    echo "An older deployment of edge-scheduler is already running - terminating it ...."
+    pkill main # edge-scheduler
+fi
+
+# Check placement-translator is already running
+if [ $(process_running placement-translator) == "running" ]
+then
+    echo "An older deployment of placement-translator is already running - terminating it ...."
+    pkill placement-translator
+fi
+
+
 
 #(1): Clone the kcp-playground tool
 echo "****************************************"
 echo "Clonining kcp-playground repo"
 echo "****************************************"
-rm -rf $(pwd)/kcp
-git clone -b kcp-playground https://github.com/fabriziopandini/kcp.git
+
+if [[ ! -d $(pwd)/kcp || ! "$(ls -A $(pwd)/kcp)" ]] 
+then
+    if [ $verbosity == 1 ]; then
+        git clone -b kcp-playground https://github.com/fabriziopandini/kcp.git
+    else
+        git clone -b kcp-playground https://github.com/fabriziopandini/kcp.git  > /dev/null 2>&1
+        echo "Finished cloning repo ......"
+    fi
+else 
+   echo "  kcp-playground repo already exists ..."
+fi
 
 #(2): Move the kcp-playground yaml files to the target repo
-mkdir $(pwd)/kcp/test/kubectl-kcp-playground/examples/kcp-edge/
-cp stages/*  $(pwd)/kcp/test/kubectl-kcp-playground/examples/kcp-edge/
+stages_path=$(pwd)/kcp/test/kubectl-kcp-playground/examples/kcp-edge/
+if [ ! -d $stages_path ] 
+then
+    mkdir $stages_path
+fi
+
+cp stages/*  $stages_path
 
 #(3): build the binaries for kcp and kcp-playground plugin
 echo "****************************************"
 echo "Building kcp-playground binaries"
 echo "****************************************"
 cd $(pwd)/kcp
-make build
+
+if [ -f "bin/kubectl-kcp-playground" ] 
+then
+    echo "  kcp-playground binaries already exists ..."
+else 
+    if [ $verbosity == 1 ]; then
+        make build
+    else
+        echo "Started building the binaries ..."
+        make build  > /dev/null 2>&1
+        echo "Finshed building the binaries ..."
+    fi
+fi
 
 #(4): Set up the kubeconfig and path variables
 kcp_path=$(pwd)/bin
@@ -53,19 +137,19 @@ export KUBECONFIG=$kubeconfig_path
 
 #(5): Start the kcp-playground
 echo "****************************************"
-echo "Started deploying kcp-playground: complete in ~ 2m30sec (maximum waiting time: 5 minutes)"
+echo "Started deploying kcp-playground: complete in ~ 150 sec (maximum waiting time: 300 sec)"
 echo "****************************************"
 rm -rf .kcp-playground/
 
 if [ $stage == 1 ]; then  
-    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage1.yml >& kcp-playground-log.txt &
+    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage1.yml >& ../kcp-playground-log.txt &
 elif [ $stage == 2 ]; then
-    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage2.yml >& kcp-playground-log.txt &
+    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage2.yml >& ../kcp-playground-log.txt &
 elif [ $stage == 3 ]; then
-    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage2.yml >& kcp-playground-log.txt &
+    kubectl kcp playground start -f test/kubectl-kcp-playground/examples/kcp-edge/poc2023q1-stage2.yml >& ../kcp-playground-log.txt &
 fi 
 
-####################################################
+#####################################################
 MAX_RETRIES=20 # maximum wait: 5 minutes
 retries=0
 sucess=1
@@ -83,11 +167,11 @@ while [ $retries -le "$MAX_RETRIES" ]; do
 
     sleep 15
     sec=$(( retries * 15 ))
-    echo "Waited $sec seconds"
+    echo "$sec sec"
 done
 
 if [ $sucess == 1 ]; then
-   echo "kcp-playground kubeconfig not generated - maximum waiting time exceeded: 5 minutes"
+   echo "kcp-playground kubeconfig not generated - maximum waiting time exceeded: 300 sec"
    exit 
 fi 
 ####################################################
@@ -95,46 +179,85 @@ echo "****************************************"
 echo "Finished deploying kcp-playground .... (log file: kcp-playground-log.txt)"
 echo "****************************************"
 
+wait_for_process(){
+  status=$(process_running $1)
+  MAX_RETRIES=5
+  retries=0
+  status_code=0
+  while [ $status != "running" ]; do
+      if [ $retries -eq $MAX_RETRIES ]; then
+           status_code=1
+           break
+      fi
+
+      retries=$(( retries + 1 ))
+      sleep 3
+      status=$(process_running $1)
+  done
+  echo $status_code
+}
+
 
 #(6): Start the edge-mc controller
 echo "****************************************"
-echo "Started deploying maibox-controller ...."
+echo "Started deploying kCP-EDGE controllers ...."
 echo "****************************************"
 cd ../../..
 
 kubectl ws root:espw
-go run ./cmd/mailbox-controller --inventory-context=shard-main-root -v=2 >& mailbox-controller-log.txt &
-echo "****************************************"
-echo "Finished deploying maibox-controller .... (log file: mailbox-controller-log.txt)"
-echo "****************************************"
+go run ./cmd/mailbox-controller --inventory-context=shard-main-root -v=2 >& environments/dev-env/mailbox-controller-log.txt &
+
+run_status=$(wait_for_process mailbox-controller)
+if [ $run_status -eq 0 ]; then
+    echo " mailbox-controller is running (log file: mailbox-controller-log.txt)"
+else
+    echo " mailbox-controller failed to start ..... exiting"
+    sleep 2
+    exit
+fi
 
 
-if [ $stage != 1 ]; then 
+if [ $stage -gt 1 ]; then 
     # (7): Start the edge-mc scheduler
-    echo "****************************************"
-    echo "Started deploying edge scheduler...."
-    echo "****************************************"
-    sleep 10
+    sleep 3
     kubectl ws root:espw
-    go run cmd/scheduler/main.go -v 2 --kcp-kubeconfig=$kubeconfig_path >& edge-scheduler-log.txt &
-    echo "****************************************"
-    echo "Finished deploying edge scheduler...."
-    echo "****************************************"
+    go run cmd/scheduler/main.go -v 2 --kcp-kubeconfig=$kubeconfig_path >& environments/dev-env/edge-scheduler-log.txt &
+    message=$(wait_for_process main)
+    
+    run_status=$(wait_for_process main)
+    if [ $run_status -eq 0 ]; then
+        echo " scheduler is running (log file: edge-scheduler-log.txt)"
+    else
+        echo " scheduler failed to start ..... exiting"
+        exit
+    fi
 fi
 
 
-if [ $stage != 1 ]; then 
+if [ $stage -gt 2 ]; then 
     # (8): Start the Placement Translator
-    echo "*****************************************"
-    echo "Started deploying placement translator.."
-    echo "*****************************************"
-    sleep 10
+    sleep 3
     kubectl ws root:espw
-    go run ./cmd/placement-translator --allclusters-context  "shard-main-system:admin" >& placement-translator-log.txt &
-    echo "*****************************************"
-    echo "Finished deploying placement translator.."
-    echo "*****************************************"
+    go run ./cmd/placement-translator --allclusters-context  "shard-main-system:admin" >& environments/dev-env/placement-translator-log.txt &
+
+    run_status=$(wait_for_process placement-translator)
+    if [ $run_status -eq 0 ]; then
+        echo " placement translator is running (log file: placement-translator-log.txt)"
+    else
+        echo " placement translator failed to start ..... exiting"
+        exit
+    fi
 fi
+
+sleep 5
+kubectl ws root
+
+echo "****************************************"
+echo "Finished deploying kCP-EDGE controllers ...."
+echo "****************************************"
+
+export PATH=$PATH:$(pwd)/kcp/bin
+export KUBECONFIG=$(pwd)/kcp/.kcp-playground/playground.kubeconfig
 
 echo "KCP-Edge dev-env successfully started"
 echo "To start using the KCP-Edge dev-env: "
