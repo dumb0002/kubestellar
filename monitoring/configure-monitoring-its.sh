95da473c5b3ee6759a6e4f18fbf0b6a61754f40a
Braulio Dumba <brauliodumba@gmail.com>
2024-08-05 18:32:25 -0400

    latest fixes
    Signed-off-by: Braulio Dumba <brauliodumba@gmail.com>

--- monitoring/configure-monitoring-its.sh
+++ monitoring/configure-monitoring-its.sh
@@ -68,7 +68,7 @@ pod_label=$(kubectl -n its1-system get pod $pod_name -o jsonpath='{.metadata.lab
 sed "s^%STATUS_CTL_LABEL%^$pod_label^g" ${SCRIPT_DIR}/configuration/status-addon-ctl-svc.yaml | kubectl -n $monitoring_ns apply -f -
 
 : 2. Adding declarations of the metrics and pprof ports, so that addon-status service definition can refer to it by name
-kubectl --context $its -n open-cluster-management get deploy addon-status-controller -o yaml | yq '(del(.status) |.spec.template.spec.containers.[0].ports[0].name |= "metrics")' | yq '.spec.template.spec.containers.[0].ports[0].protocol |= "TCP"' | yq '.spec.template.spec.containers.[0].ports[0].containerPort |= 8090' | yq '.spec.template.spec.containers.[0].ports[1].name |= "pprof"' | yq '.spec.template.spec.containers.[0].ports[1].protocol |= "TCP"' | yq '.spec.template.spec.containers.[0].ports[1].containerPort |= 8092' | kubectl --context $ctx apply --namespace=open-cluster-management -f -
+kubectl --context $its -n open-cluster-management get deploy addon-status-controller -o yaml | yq '(del(.status) |.spec.template.spec.containers.[0].ports[0].name |= "metrics")' | yq '.spec.template.spec.containers.[0].ports[0].protocol |= "TCP"' | yq '.spec.template.spec.containers.[0].ports[0].containerPort |= 8090' | yq '.spec.template.spec.containers.[0].ports[1].name |= "pprof"' | yq '.spec.template.spec.containers.[0].ports[1].protocol |= "TCP"' | yq '.spec.template.spec.containers.[0].ports[1].containerPort |= 8092' | kubectl --context $its apply --namespace=open-cluster-management -f -
 
 : 3. Create the service monitor:
 sed s/%ITS_NS%/$its-system/g ${SCRIPT_DIR}/configuration/status-addon-ctl-sm.yaml | kubectl -n $monitoring_ns apply -f -
